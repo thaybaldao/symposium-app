9b7f5a74a78fd31f1c3117fc4b19409d4d5c6197
thaybaldao <thayna.baldao@gmail.com>
2020-10-18 23:08:57 -0300

    deleting unused css files

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header data={this.state.resumeData.main}/>
+        <Header />
       </div>
     );
   }
