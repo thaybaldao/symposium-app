fc5e9b3fe12f8be65793c0b1c46d0fcacb1f66b6
thaybaldao <thayna.baldao@gmail.com>
2020-10-21 19:59:00 -0300

    target blank

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
 
     if(this.props.data){
       var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+        return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
       })
       var supporters= this.props.data.support.map(function(support){
         var path = "../Images/"+ support.image
@@ -22,9 +22,9 @@ class Footer extends Component {
            </ul>
            <p style={{margin: "0 0 -50px 0", position: "relative", right: "5px"}}> APOIADORES </p>
            <ul className="copyright" style={{position: "relative", right: "30px"}}>
-              <li><a href={"http://www.ita.br/"}><img className="ita" src={require('../Images/ita.png')} alt="ita"/></a></li>
-              <li><a href={"https://itaex.com.br/"}><img className="itaex" src={require('../Images/itaex.png')} alt="itaex"/></a></li>
-              <li><a href={"https://www.jnjbrasil.com.br/"}><img className="johnson" src={require('../Images/jj.png')} alt="jnjbrasil"/></a></li>
+              <li><a href={"http://www.ita.br/"} target="_blank"><img className="ita" src={require('../Images/ita.png')} alt="ita"/></a></li>
+              <li><a href={"https://itaex.com.br/"} target="_blank"><img className="itaex" src={require('../Images/itaex.png')} alt="itaex"/></a></li>
+              <li><a href={"https://www.jnjbrasil.com.br/"} target="_blank"><img className="johnson" src={require('../Images/jj.png')} alt="jnjbrasil"/></a></li>
            </ul>
         <p></p>
         </div>
